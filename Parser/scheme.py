ba76e1e3c6d48679e2460b94eabdbc85f2baa713
BOUTRIKmohamed <boutrikmohamedpro@outlook.com>
2023-11-30 18:14:07 +0100

    parser_updated_chunk

--- Parser/scheme.py
+++ Parser/scheme.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import psycopg2
+import os.path
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql://l3info_42:L3INFO_42@10.11.11.22/l3info_42")
+engine = create_engine("postgresql://transport:transport@localhost/transport")
 conn = engine.connect()
-
-conn2 = psycopg2.connect(database="l3info_42", user="l3info_42", host="10.11.11.22", password="L3INFO_42")
-cursor = conn2.cursor()
-
   
 #paris_walk
-df = pd.read_csv("network_walk.csv",sep=';')
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_walk.csv",sep=';')
 df.to_sql(name='paris_walk',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_walk""")
 # conn2.commit()
@@ -18,7 +15,7 @@ df.to_sql(name='paris_walk',con=conn,if_exists='replace')
 # print(f"walk : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
 #paris_bus
-df = pd.read_csv("network_bus.csv",sep=';')
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_bus.csv",sep=';')
 df.to_sql(name='paris_bus',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_bus""")
 # conn2.commit()
@@ -26,7 +23,7 @@ df.to_sql(name='paris_bus',con=conn,if_exists='replace')
 # print(f"bus : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
 #paris_rail
-df = pd.read_csv("network_rail.csv",sep=';')
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_rail.csv",sep=';')
 df.to_sql(name='paris_rail',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_rail""")
 # conn2.commit()
@@ -34,7 +31,7 @@ df.to_sql(name='paris_rail',con=conn,if_exists='replace')
 # print(f"rail : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
 #paris_subway
-df = pd.read_csv("network_subway.csv",sep=';')
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_subway.csv",sep=';')
 df.to_sql(name='paris_subway',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_subway""")
 # conn2.commit()
@@ -42,7 +39,7 @@ df.to_sql(name='paris_subway',con=conn,if_exists='replace')
 # print(f"subway : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
 #paris_nodes
-df = pd.read_csv("network_nodes.csv",sep=';')
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_nodes.csv",sep=';')
 df.to_sql(name='paris_nodes',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_nodes""")
 # conn2.commit()
@@ -50,7 +47,7 @@ df.to_sql(name='paris_nodes',con=conn,if_exists='replace')
 # print(f"nodes : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
 #paris_combined
-df = pd.read_csv("network_combined.csv",sep=';')
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_combined.csv",sep=';')
 df.to_sql(name='paris_combined',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_combined""")
 # conn2.commit()
@@ -58,23 +55,30 @@ df.to_sql(name='paris_combined',con=conn,if_exists='replace')
 # print(f"combined : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
 #paris_temporal_day
-df = pd.read_csv("network_temporal_day.csv",sep=';')
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_temporal_day.csv",sep=';')
 df.to_sql(name='paris_temporal_day',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_temporal_day""")
 # conn2.commit()
 # rows = cursor.fetchall()
 # print(f"temporal_day : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
+
 #paris_temporal_week
-df = pd.read_csv("network_temporal_week.csv",sep=';')
-df.to_sql(name='paris_temporal_week',con=conn,if_exists='replace')
+reader = pd.read_csv(os.path.abspath('.')+"/paris/network_temporal_week.csv",sep=';',chunksize=1000000)
+#size = 0
+for rows in reader : 
+   df = pd.DataFrame(rows)
+   df.to_sql(name='paris_temporal_week',con=conn,if_exists='append')
+   #size +=df.shape[0]
+#print(size)   
 # cursor.execute("""SELECT count(*) from paris_temporal_week""")
 # conn2.commit()
 # rows = cursor.fetchall()
 # print(f"temporal_week : existed in data_frame {df.shape[0]} inserted {rows[0][0]}")
 
 #paris_tram
-df = pd.read_csv("network_tram.csv",sep=';')
+
+df = pd.read_csv(os.path.abspath('.')+"/paris/network_tram.csv",sep=';')
 df.to_sql(name='paris_tram',con=conn,if_exists='replace')
 # cursor.execute("""SELECT count(*) from paris_tram""")
 # conn2.commit()
